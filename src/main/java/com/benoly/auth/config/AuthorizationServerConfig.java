60fd2c84ac8c284876ca79ee19ac1c92d4dbcb21
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2020-08-20 15:21:32 +0100

    Added SessionInvalidatingHandlerInterceptor for Authorization code flow

--- src/main/java/com/benoly/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/benoly/auth/config/AuthorizationServerConfig.java
@@ -1,10 +1,10 @@
 package com.benoly.auth.config;
 
+import com.benoly.auth.repository.AuthorizationTokenRepository;
 import com.benoly.auth.service.ClientService;
 import com.benoly.auth.service.UserService;
-import com.sun.el.parser.Token;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
+import com.benoly.auth.tokenservices.CustomAuthorizationCodeServices;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,31 +15,36 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    private final ClientService clientService;
-    private final AuthenticationManager authenticationManager;
     private final UserService userService;
-    private final AuthorizationServerTokenServices tokenServices;
+    private final ClientService clientService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final AuthorizationTokenRepository authorizationTokenRepository;
+    private final AuthorizationServerTokenServices tokenServices;
 
     public AuthorizationServerConfig(ClientService clientService,
                                      AuthenticationConfiguration authenticationConfiguration,
                                      AuthorizationServerTokenServices tokenServices,
                                      PasswordEncoder passwordEncoder,
+                                     AuthorizationTokenRepository authorizationTokenRepository,
                                      UserService userService) throws Exception {
-        this.clientService = clientService;
-        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
-        this.tokenServices = tokenServices;
-        this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+        this.tokenServices = tokenServices;
+        this.clientService = clientService;
+        this.passwordEncoder = passwordEncoder;
+        this.authorizationTokenRepository = authorizationTokenRepository;
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
     }
 
     @Override
@@ -59,6 +64,35 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.
                 authenticationManager(authenticationManager)
                 .userDetailsService(userService)
+                .authorizationCodeServices(new CustomAuthorizationCodeServices(authorizationTokenRepository))
                 .tokenServices(tokenServices);
+
+        endpoints.addInterceptor(new SessionInvalidatingHandlerInterceptor());
     }
+
+
+}
+
+/**
+ * This is interceptor invalidates sessions after the Authorization code flow is complete
+ * To ensure the Authorization server is stateless
+ */
+@Slf4j
+class SessionInvalidatingHandlerInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        if (modelAndView != null && modelAndView.getView() instanceof RedirectView) {
+            RedirectView redirectView = (RedirectView) modelAndView.getView();
+            String redirectUrl = redirectView.getUrl();
+            if (redirectUrl == null) return;
+            if (redirectUrl.contains("code=") || redirectUrl.contains("error=") ) {
+                HttpSession session = request.getSession(false);
+                if (session != null) {
+                    log.debug("invalidating session {}", session.getId());
+                    session.invalidate();
+                }
+            }
+        }
+     }
 }
