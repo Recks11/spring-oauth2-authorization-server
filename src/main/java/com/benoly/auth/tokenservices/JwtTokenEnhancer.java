8818ac4d647ceb5d3fdf6c323371393de213053c
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2020-08-21 02:26:42 +0100

    updated public key

--- src/main/java/com/benoly/auth/tokenservices/JwtTokenEnhancer.java
+++ src/main/java/com/benoly/auth/tokenservices/JwtTokenEnhancer.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.provider.token.store.IssuerClaimVerifier;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtClaimsSetVerifier;
-import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
 
 public class JwtTokenEnhancer extends JwtAccessTokenConverter {
     @Value("${auth.jwt.issuer:https://rexijie.dev}")
@@ -19,5 +21,18 @@ public class JwtTokenEnhancer extends JwtAccessTokenConverter {
     public void setJwtClaimsSetVerifier(JwtClaimsSetVerifier jwtClaimsSetVerifier) {
         var issuerUrl = new URL(issuer);
         super.setJwtClaimsSetVerifier(new IssuerClaimVerifier(issuerUrl));
+    }
+
+    @Override
+    public void setKeyPair(KeyPair keyPair) {
+        super.setKeyPair(keyPair);
+        this.setVerifierKey(generateRSAString(keyPair));
+    }
+
+    protected String generateRSAString(KeyPair keyPair) {
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        return "-----BEGIN PUBLIC KEY-----"
+                + Base64.getEncoder().encodeToString(publicKey.getEncoded())
+                + "-----END PUBLIC KEY-----";
     }
 }
