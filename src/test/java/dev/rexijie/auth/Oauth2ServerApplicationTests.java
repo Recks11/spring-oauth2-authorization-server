0aa3ba9faf444783ab7bfd5151e6144431ff4a95
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-09-07 20:20:37 +0100

    updated tests

--- src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
+++ src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
@@ -3,6 +3,8 @@ package dev.rexijie.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//TODO: write all tests
+
 @SpringBootTest
 class Oauth2ServerApplicationTests {
 
