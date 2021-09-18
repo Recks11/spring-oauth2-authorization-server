516596fc400b9217a1a728d87db835e31aed6ea2
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-09-18 15:10:11 +0100

    Switched to SpringBootTest

--- src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
+++ src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
@@ -1,8 +1,9 @@
 package dev.rexijie.auth;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -10,7 +11,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
 class Oauth2ServerApplicationTests {
 
 	@Container
