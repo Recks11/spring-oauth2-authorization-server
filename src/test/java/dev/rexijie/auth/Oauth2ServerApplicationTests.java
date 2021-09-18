b3fbe84c7ad8f4c60708ccd935d3220a8bcbf11e
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-09-18 14:52:33 +0100

    Removed mongo tests for jenkins

--- src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
+++ src/test/java/dev/rexijie/auth/Oauth2ServerApplicationTests.java
@@ -2,18 +2,23 @@ package dev.rexijie.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-//@Testcontainers
+@Testcontainers
 @SpringBootTest
 class Oauth2ServerApplicationTests {
 
-//	@Container
-//	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4");
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4");
 
-//	@DynamicPropertySource
-//	static void setProperties(DynamicPropertyRegistry registry) {
-//		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-//	}
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
